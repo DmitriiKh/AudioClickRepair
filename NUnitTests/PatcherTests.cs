b02f52385057e3aedb108c728f17b3189303b815
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-04-25 16:01:10 -0300

    Add test for Patcher (two patches)

--- NUnitTests/PatcherTests.cs
+++ NUnitTests/PatcherTests.cs
@@ -26,6 +26,7 @@ namespace NUnitTests
             }
 
             public int StartPosition { get; private set; }
+
             public int Length { get; private set; }
 
             public int GetEndPosition() => StartPosition + Length - 1;
@@ -58,21 +59,13 @@ namespace NUnitTests
         [TestCase(200, 100, 195, 10)] // patch covers ending of range
         [TestCase(200, 100, 245, 10)] // patch outside of range (right)
         [TestCase(200, 100, 95, 110)] // patch covers entire range
-        public void RangeData_GetRangeBefore_ReturnsCorrectRange(
+        public void RangeData_GetRangeBefore_OnePatch_ReturnsCorrectRange(
             int positionExcluding, 
             int length,
             int patchStart,
             int patchLength)
         {
-            var patch = new PatchForTest(
-                patchStart,
-                patchLength);
-
-            for (var position = patch.StartPosition; 
-                position <= patch.GetEndPosition(); 
-                position++)
-                // Change sign to opposite
-                patch.SetOutputSample(position, -_arrayValueEqualsIndex[position]);
+            var patch = CreatePatchChangedSign(patchStart, patchLength);
 
             _patchCollection.Add(patch);
 
@@ -88,6 +81,53 @@ namespace NUnitTests
                    
                 Assert.AreEqual(expected, range.GetValue(position));
             }
+        }
+
+        [TestCase(200, 100, 110, 10, 150, 10)]
+        public void RangeData_GetRangeBefore_TwoPatches_ReturnsCorrectRange(
+            int positionExcluding,
+            int length,
+            int patchStartFirst,
+            int patchLengthFirst,
+            int patchStartSecond,
+            int patchLengthSecond)
+        {
+            var firstPatch = CreatePatchChangedSign(patchStartFirst, patchLengthFirst);
+            _patchCollection.Add(firstPatch);
+
+            var secondPatch = CreatePatchChangedSign(patchStartSecond, patchLengthSecond);
+            _patchCollection.Add(secondPatch);
+
+            var range = _patcher.GetRangeBefore(positionExcluding, length);
+
+            for (var position = range.StartPosition;
+                position <= range.EndPosition;
+                position++)
+            {
+                var patchedFirst = position >= firstPatch.StartPosition &&
+                    position <= firstPatch.GetEndPosition();
+                var patchedSecond = position >= secondPatch.StartPosition &&
+                    position <= secondPatch.GetEndPosition();
+
+                var expected = patchedFirst || patchedSecond ? -position : position;
+
+                Assert.AreEqual(expected, range.GetValue(position));
+            }
+        }
+
+        private PatchForTest CreatePatchChangedSign(int patchStart, int patchLength)
+        {
+            var patch = new PatchForTest(
+                patchStart,
+                patchLength);
+
+            for (var position = patch.StartPosition;
+                position <= patch.GetEndPosition();
+                position++)
+                // Change sign to opposite
+                patch.SetOutputSample(position, -_arrayValueEqualsIndex[position]);
+
+            return patch;
         }
     }
 }
