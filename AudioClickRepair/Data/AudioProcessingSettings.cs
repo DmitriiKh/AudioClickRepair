abee884b02884f9fdc544fae6a8a98b1f0c56c37
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-16 13:56:55 -0300

    Document AudioProcessingSettings class

--- AudioClickRepair/Data/AudioProcessingSettings.cs
+++ AudioClickRepair/Data/AudioProcessingSettings.cs
@@ -1,23 +1,38 @@
-﻿namespace AudioClickRepair.Data
+﻿// <copyright file="AudioProcessingSettings.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace AudioClickRepair.Data
 {
+    /// <summary>
+    /// Set of settings for audio data.
+    /// </summary>
     public class AudioProcessingSettings : IAudioProcessingSettings
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioProcessingSettings"/> class.
+        /// </summary>
         public AudioProcessingSettings()
         {
-            HistoryLengthSamples = 512;
-            CoefficientsNumber = 4;
-            ThresholdForDetection = 10;
-            MaxLengthOfCorrection = 250;
+            this.HistoryLengthSamples = 512;
+            this.CoefficientsNumber = 4;
+            this.ThresholdForDetection = 10;
+            this.MaxLengthOfCorrection = 250;
         }
 
+        /// <inheritdoc/>
         public int HistoryLengthSamples { get; set; }
 
+        /// <inheritdoc/>
         public int CoefficientsNumber { get; set; }
 
+        /// <inheritdoc/>
         public double ThresholdForDetection { get; set; }
 
+        /// <inheritdoc/>
         public int MaxLengthOfCorrection { get; set; }
 
+        /// <inheritdoc/>
         public int SampleRate { get; set; } = -1;
     }
 }
