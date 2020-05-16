4e9647885446c333622e313179c83db7358f5d80
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-16 12:54:07 -0300

    Document IPatcher interface.

--- AudioClickRepair/Processing/IPatcher.cs
+++ AudioClickRepair/Processing/IPatcher.cs
@@ -1,9 +1,26 @@
 ﻿namespace AudioClickRepair.Processing
 {
+    /// <summary>
+    /// Interface of a Patcher.
+    /// </summary>
     public interface IPatcher
     {
+        /// <summary>
+        /// Gets array containing sequence of patched samples of specified length
+        /// starting from the specified position.
+        /// </summary>
+        /// <param name="start">Start position of range.</param>
+        /// <param name="length">Length of range.</param>
+        /// <param name="anotherPatch">One more optional AbstractPatch
+        /// that is not in the Collection yet.</param>
+        /// <returns>Array of patched samples.</returns>
         double[] GetRange(int start, int length, Data.AbstractPatch anotherPatch = null);
 
+        /// <summary>
+        /// Returns patched value of a sample.
+        /// </summary>
+        /// <param name="position">Position of the required sample.</param>
+        /// <returns>Value of the sample.</returns>
         double GetValue(int position);
     }
 }
