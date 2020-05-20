06c4a97a31a4fbc01f9b4f111aa461fab94f7915
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-20 07:42:50 -0300

    Document IDetector

--- AudioClickRepair/Processing/IDetector.cs
+++ AudioClickRepair/Processing/IDetector.cs
@@ -1,11 +1,28 @@
-﻿namespace AudioClickRepair.Processing
+﻿// <copyright file="IDetector.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace AudioClickRepair.Processing
 {
     using AudioClickRepair.Data;
 
+    /// <summary>
+    /// Detects prediction error level.
+    /// </summary>
     public interface IDetector
     {
+        /// <summary>
+        /// Gets size of input data needed to perform calculations.
+        /// </summary>
         int InputDataSize { get; }
 
+        /// <summary>
+        /// Returns prediction error level for a sample at specific position.
+        /// </summary>
+        /// <param name="position">Position of sample.</param>
+        /// <param name="anotherPatch">Additional patch that is not included
+        /// in the patch collection (optional).</param>
+        /// <returns>Prediction error level.</returns>
         double GetErrorLevel(int position, AbstractPatch anotherPatch = null);
     }
 }
