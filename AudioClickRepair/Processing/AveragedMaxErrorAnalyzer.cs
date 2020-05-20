637266723130f8e3ebd1dd1552645fd7a8d3bca7
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-20 08:09:18 -0300

    Document AveragedMaxErrorAnalyzer class

--- AudioClickRepair/Processing/AveragedMaxErrorAnalyzer.cs
+++ AudioClickRepair/Processing/AveragedMaxErrorAnalyzer.cs
@@ -1,35 +1,48 @@
-﻿namespace AudioClickRepair.Processing
+﻿// <copyright file="AveragedMaxErrorAnalyzer.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace AudioClickRepair.Processing
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Analyzes errors. Finds maximums for each block of errors and than averages them.
+    /// </summary>
     public class AveragedMaxErrorAnalyzer : IAnalyzer
     {
-        private const int _blockSize = 16;
-        private const int _blocksNumber = 16;
+        private const int BlockSize = 16;
+        private const int BlocksNumber = 16;
 
-        public int InputDataSize => _blockSize * _blocksNumber;
+        /// <inheritdoc/>
+        public int InputDataSize => BlockSize * BlocksNumber;
 
+        /// <inheritdoc/>
         public double DefaultResult => 0;
 
+        /// <inheritdoc/>
         public double GetResult(double[] errors)
         {
-            if (errors.Length != InputDataSize)
-                throw new ArgumentException("Not correct length of " + nameof(errors));
+            if (errors is null)
+            {
+                throw new ArgumentNullException(nameof(errors));
+            }
 
-            return Slice(errors)
+            return this.Slice(errors)
                 .Select(block => block.Select(Math.Abs).Max())
                 .Average();
         }
 
         private IEnumerable<double[]> Slice(double[] array)
         {
-            for (int blockStart = 0, blockEndExcluding = _blockSize;
+            for (int blockStart = 0, blockEndExcluding = BlockSize;
                 blockEndExcluding <= array.Length;
-                blockStart += _blockSize, blockEndExcluding += _blockSize)
-
+                blockStart += BlockSize, blockEndExcluding += BlockSize)
+            {
                 yield return array[blockStart..blockEndExcluding];
+            }
         }
     }
 }
