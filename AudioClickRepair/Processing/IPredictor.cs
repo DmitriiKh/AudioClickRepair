c81c25bb0dee2c1cfff1e5c3db1aef313d0ccb7b
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-20 07:26:40 -0300

    Document IPredictor

--- AudioClickRepair/Processing/IPredictor.cs
+++ AudioClickRepair/Processing/IPredictor.cs
@@ -1,11 +1,31 @@
-﻿namespace AudioClickRepair.Processing
+﻿// <copyright file="IPredictor.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace AudioClickRepair.Processing
 {
+    /// <summary>
+    /// Gives predictions for a sequence of samples.
+    /// </summary>
     public interface IPredictor
     {
+        /// <summary>
+        /// Gets number of samples needed to make calculations.
+        /// </summary>
         int InputDataSize { get; }
 
+        /// <summary>
+        /// Returns forward prediction for the sequence of samples.
+        /// </summary>
+        /// <param name="samples">Sequence of samples.</param>
+        /// <returns>Forward prediction.</returns>
         double GetForward(double[] samples);
 
+        /// <summary>
+        /// Returns backward prediction for the sequence of samples.
+        /// </summary>
+        /// <param name="samples">Sequence of samples.</param>
+        /// <returns>Backward prediction.</returns>
         double GetBackward(double[] samples);
     }
 }
