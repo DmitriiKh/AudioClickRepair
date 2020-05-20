917d488dab20c7cc2fc61b54af07918f86f767f8
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-20 06:00:10 -0300

    Document IRegenerator

--- AudioClickRepair/Processing/IRegenerator.cs
+++ AudioClickRepair/Processing/IRegenerator.cs
@@ -1,11 +1,25 @@
-﻿namespace AudioClickRepair.Processing
+﻿// <copyright file="IRegenerator.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace AudioClickRepair.Processing
 {
     using AudioClickRepair.Data;
 
-    interface IRegenerator
+    /// <summary>
+    /// Regenerates sequence of audio samples.
+    /// </summary>
+    internal interface IRegenerator
     {
+        /// <summary>
+        /// Gets number of input samples needed in order to make regeneration.
+        /// </summary>
         int InputDataSize { get; }
 
+        /// <summary>
+        /// Restores the sequence of audio samples inside patch.
+        /// </summary>
+        /// <param name="patch">Patch that needs restoration.</param>
         void RestorePatch(AbstractPatch patch);
     }
 }
