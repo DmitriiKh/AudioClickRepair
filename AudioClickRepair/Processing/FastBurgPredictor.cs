4e7c85b2125326dc09ad84da7b5b2953732963d7
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-20 07:35:24 -0300

    Document FastBurgPredictor class.

--- AudioClickRepair/Processing/FastBurgPredictor.cs
+++ AudioClickRepair/Processing/FastBurgPredictor.cs
@@ -4,19 +4,31 @@
 
 namespace AudioClickRepair.Processing
 {
+    /// <summary>
+    /// Gives predictions for a sequence of samples using Fast Burg Algorithm.
+    /// </summary>
     internal class FastBurgPredictor : IPredictor
     {
         private readonly int coefficientsNumber;
         private readonly int historyLength;
 
-        public int InputDataSize => this.historyLength;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastBurgPredictor"/> class.
+        /// </summary>
+        /// <param name="coefficientsNumber">Number of coefficients for
+        /// the Burg algorithm.</param>
+        /// <param name="historyLength">Number of samples used as input
+        /// for training the Burg Algorithm.</param>
         public FastBurgPredictor(int coefficientsNumber, int historyLength)
         {
             this.coefficientsNumber = coefficientsNumber;
             this.historyLength = historyLength;
         }
 
+        /// <inheritdoc/>
+        public int InputDataSize => this.historyLength;
+
+        /// <inheritdoc/>
         public double GetForward(double[] samples)
         {
             var fastBurgAlgorithm = new FastBurgAlgorithm64(samples);
@@ -28,6 +40,7 @@ namespace AudioClickRepair.Processing
             return fastBurgAlgorithm.GetForwardPrediction();
         }
 
+        /// <inheritdoc/>
         public double GetBackward(double[] samples)
         {
             var fastBurgAlgorithm = new FastBurgAlgorithm64(samples);
