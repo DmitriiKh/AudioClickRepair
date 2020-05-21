32e7ddadbf28cc6385e61be7ec630f48a16834d3
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-21 12:03:05 -0300

    Add unit tests for Mono class

---
+++ NUnitTests/MonoTests.cs
@@ -0,0 +1,29 @@
+﻿using CarefulAudioRepair.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace NUnitTests
+{
+    public class MonoTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(0)]
+        [TestCase(10)]
+        public void Mono_ScansArray_DoNotThrow(int inputLength)
+        {
+            var zeros = new double[inputLength];
+            var audio = new Mono(
+                zeros,
+                new AudioProcessingSettings() { HistoryLengthSamples = 512 });
+
+            Assert.DoesNotThrowAsync(
+                () => audio.ScanAsync(new Progress<string>(), new Progress<double>()));
+        }
+    }
+}
