9641243c280867ac34c8fef9058226f501f51bab
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-26 07:51:36 -0300

    Add IScanner interface

---
+++ CarefulAudioRepair/Processing/IScanner.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IScanner.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace CarefulAudioRepair.Processing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    interface IScanner
+    {
+    }
+}
