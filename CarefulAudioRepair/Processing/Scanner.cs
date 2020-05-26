4812cb7eaadfa5e151ffd1565a2792ad4154dec3
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-26 07:53:53 -0300

    Add Scanner class

---
+++ CarefulAudioRepair/Processing/Scanner.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Scanner.cs" company="Dmitrii Khrustalev">
+// Copyright (c) Dmitrii Khrustalev. All rights reserved.
+// </copyright>
+
+namespace CarefulAudioRepair.Processing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Scanner : IScanner
+    {
+    }
+}
