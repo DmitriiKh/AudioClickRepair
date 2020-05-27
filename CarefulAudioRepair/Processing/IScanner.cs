3b238f3cd18c33db8aa28f0087ee75a4baf03138
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-05-27 14:58:31 -0300

    Document IScanner

--- CarefulAudioRepair/Processing/IScanner.cs
+++ CarefulAudioRepair/Processing/IScanner.cs
@@ -7,8 +7,18 @@ namespace CarefulAudioRepair.Processing
     using System;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Interface for Scanner
+    /// </summary>
     internal interface IScanner
     {
+        /// <summary>
+        /// Scans audio using ScannerTools.
+        /// Changes ScannerTools.PatchCollection and ScannerTools.PredictionErrPatcher.
+        /// </summary>
+        /// <param name="status">Parameter to report status through.</param>
+        /// <param name="progress">Parameter to report progress through.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
         public Task<ScannerTools> ScanAsync(
             IProgress<string> status,
             IProgress<double> progress);
